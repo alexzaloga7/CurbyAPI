a0b74f889fb55c5184ed64f7a683a11bb64782db
Alex Zaloga <alexandrazaloga@gmail.com>
2023-05-07 22:04:01 -0400

    added upload functionality

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-require("dotenv").config();
+const videodetails = require("./data/video-details.json");
+const videoRoutes = require("./routes/videos");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const fs = require("fs");
+const port = process.env.PORT || process.argv[2] || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("welcome to /");
-});
+app.use(express.static("public"));
+app.use("/videos", videoRoutes);
 
-app.listen(8080, () => {
-  console.log(`Server started on port 8080`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
